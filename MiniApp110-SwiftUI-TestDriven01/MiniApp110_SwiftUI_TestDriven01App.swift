ab0bf786299cbf824b734a10ef8d1856b0d3f05c
Kota <maedakoutaa@gmail.com>
2022-11-23 13:23:59 +0900

    Initial Commit

---
+++ MiniApp110-SwiftUI-TestDriven01/MiniApp110_SwiftUI_TestDriven01App.swift
@@ -0,0 +1,17 @@
+//
+//  MiniApp110_SwiftUI_TestDriven01App.swift
+//  MiniApp110-SwiftUI-TestDriven01
+//
+//  Created by 前田航汰 on 2022/11/23.
+//
+
+import SwiftUI
+
+@main
+struct MiniApp110_SwiftUI_TestDriven01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
